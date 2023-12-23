e357f63bb849bc8989dccb1a8c37f1dd045c165c
Kanwal <kpsingh0801@gmail.com>
2023-12-23 15:00:21 +0530

    Add: changes for header

---
+++ src/Components/StyledComponents/styledheader.tsx
@@ -0,0 +1,22 @@
+import styled, { css } from 'styled-components';
+import { headThemeColor, laptop } from './constants';
+
+export const PageHeader = styled.div`
+	background: ${headThemeColor};
+	display: flex;
+	align-items: center;
+	height: 3.5rem;
+	.headerLogo {
+		width: 20rem;
+		height: 2rem;
+		margin-left: 10rem;
+	}
+	${laptop(css`
+		height: 49px;
+		.headerLogo {
+			width: 9rem;
+			margin-left: 1.2rem;
+			height: 1rem;
+		}
+	`)}
+`;
